0dfdaa35632371521d69ffbed7a5c91249740404
Sumit Kumar <sk4390789@gmail.com>
2023-06-15 19:17:19 +0530

    testing

--- server/handler/messageHandler.js
+++ server/handler/messageHandler.js
@@ -1,7 +1,9 @@
-const userSocketMap = require("../db/user.socket.instance");
+const socketInstances = require("../db/user.socket.instance");
+const { makeUsername } = require("../utils/shared.module");
 
 const socketMessageSend = function (input) {
     console.log("Send", input);
+    socketInstances.getSocketInstance('648840483dbccd58435d899d').emit("648840483dbccd58435d899d", makeUsername(5))
 };
 
 const socketMessageDelete = function (input) {
