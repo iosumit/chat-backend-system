375ed33a1580d07d718d2bd764e682472d3f90c1
Aditya Anand <aditechdevofficial@gmail.com>
2023-06-24 15:50:15 +0530

    :zap: Smalll change

--- server/controller/channel.controller.js
+++ server/controller/channel.controller.js
@@ -10,7 +10,7 @@ const getChannels = (req, res, next) => {
 }
 
 // Create Channels
-const createChannels = (req, res, next) => { 
+const createChannels = (req, res, next) => {
     if (!req.body || !req.body.name || !req.body.type) {
         return res.status(400).json({
             status: ERROR,
@@ -18,21 +18,21 @@ const createChannels = (req, res, next) => {
         });
     }
 
-    if (req.body.type == CHANNEL_TYPE.single && req.body.participated_users.legth > 1) {
-        return res.status(400).json({
-            status: ERROR,
-            message: strings.Multiple_user_not_allowed_for_SINGLE_CHANNEL,
-        });
-        
-    }
+
     const inputs = {
         name: req.body.name,
         participated_users: [...req.body.participated_users, req.user._id],
         type: req.body.type,
         profile_image_url: req.body.profile_image_url,
     }
+    if (inputs.type == CHANNEL_TYPE.single && inputs.participated_users.length > 2) {
+        return res.status(400).json({
+            status: ERROR,
+            message: strings.Multiple_user_not_allowed_for_SINGLE_CHANNEL,
+        });
 
-    ChannelHandler.createChannels(inputs, (err, result) => { 
+    }
+    ChannelHandler.createChannels(inputs, (err, result) => {
 
         if (err) {
             res.status(500)
