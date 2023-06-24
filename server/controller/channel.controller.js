5477d887aca429e2be385679b6038752da8aa434
Aditya Anand <aditechdevofficial@gmail.com>
2023-06-24 12:17:03 +0530

    :zap: Small change

--- server/controller/channel.controller.js
+++ server/controller/channel.controller.js
@@ -1,8 +1,18 @@
 const { SUCCESS } = require("../utils/consts");
+const channelSchema = require('../model/channel');
 
+// Get Channels
 const getChannels = (req, res, next) => {
     return res.status(200).json({ status: SUCCESS });
 }
+
+// Create Channels
+
+
+// Delete Channels
+
+// Get Single channel
+
 
 
 
